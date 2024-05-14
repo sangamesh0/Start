3783935503ad5e954970f1b6a1b60c7f72e043bb
sangu <01fe22bca107>
2024-05-15 01:30:36 +0530

    Sangu

---
+++ Addition2.java
@@ -0,0 +1,11 @@
+public class Addition2 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        int num3 = 20;
+
+        int sum = num1 + num2 + num3;
+
+        System.out.println("The sum of " + num1 + ", " + num2 + " and " + num3 + " is " + sum);
+    }
+}
